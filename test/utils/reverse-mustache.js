e7406c7ce1d1e74608e3ac326d2c9616ef7c3df9
Todd Wolfson <todd@twolfson.com>
2014-05-24 22:37:04 -0700

    Deleting virtually everything

--- test/utils/reverse-mustache.js
+++ test/utils/reverse-mustache.js
@@ -1,6 +1,6 @@
 // Load in libarary
 var expect = require('chai').expect;
-var mustache = require('mustache');
+// var mustache = require('mustache');
 var reverseMustache = require('../../');
 
 // Define helper utilities
@@ -8,11 +8,12 @@ exports.save = function (params) {
   before(function callReverseMustache () {
     this.result = reverseMustache(params);
 
-    // For behavioral sanity, verify `result + template` matches `content`
-    if (this.result) {
-      var actualOutput = mustache.render(params.template, this.result, params.partials);
-      expect(actualOutput).to.equal(params.content);
-    }
+    // TODO: Add this back
+    // // For behavioral sanity, verify `result + template` matches `content`
+    // if (this.result) {
+    //   var actualOutput = mustache.render(params.template, this.result, params.partials);
+    //   expect(actualOutput).to.equal(params.content);
+    // }
   });
   after(function cleanupResult () {
     delete this.result;
